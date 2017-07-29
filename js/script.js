2f6cc1b1b8af096cb8ef33783754b45ede7f2c0d
Neville Oronni <nevooronni@gmail.com>
2017-07-29 19:09:06 +0300

    added new user interface

--- js/script.js
+++ js/script.js
@@ -1,9 +1,66 @@
- var add = function(number1, number2) {
+//My javascript
+
+//This is the backend code
+
+var add = function(number1, number2) {
         return number1 + number2;
       };
 
-      var number1 = parseInt(prompt("Enter a number:"));
-      var number2 = parseInt(prompt("Enter another number:"));
+var subtract = function(number1, number2) {
+	return number1 - number2;};
 
-      alert(add(number1, number2);
-    
+var multiply = function(number1, number2) {
+	return number1 * number2;
+};
+
+var divide = function(number1, number2)	{
+	return number1/number2;
+};
+
+
+//This is the font-end/user interface code
+
+ $(document).ready(function() {
+ 		$("form#add").submit(function(event) {
+ 			event.preventDefault();
+ 			var number1 = parseInt($("#add1").val());
+      var number2 = parseInt($("#add2").val());
+
+      var result = add(number1, number2);
+
+      $("#output").text(result);
+ 		});
+
+ 		//subtract front end fucntion  
+ 		$("form#subtract").submit(function(event) {
+ 			event.preventDefault();
+ 			var number1 = parseInt($("#sub1").val());
+      var number2 = parseInt($("#sub2").val());
+
+      var result = subtract(number1, number2);
+
+      $("#output2").text(result);
+ 		});
+
+    //multiply front end function
+ 		$("form#multiply").submit(function(event) {
+ 			event.preventDefault();
+ 			var number1 = parseInt($("#multi1").val());
+      var number2 = parseInt($("#multi2").val());
+
+      var result = multiply(number1, number2);
+
+      $("#output3").text(result);
+ 		});
+
+ 		//division front end function	
+ 		$("form#divide").submit(function(event) {
+ 			event.preventDefault();
+ 			var number1 = parseInt($("#div1").val());
+      var number2 = parseInt($("#div2").val());
+
+      var result = divide(number1, number2);
+
+      $("#output4").text(result);
+ 		});
+ });   
